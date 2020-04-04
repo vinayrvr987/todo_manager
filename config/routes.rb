43ce89618c83ae31b19e10fa3e7451a56a99589b
vinayrvr987 <vinayrvr987@gmail.com>
2020-04-04 17:47:29 +0530

    Resources were added to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get "todos", to: "todos#index"
-  post "todos/create", to: "todos#create"
-  get "todos/:id", to: "todos#show"
+  #get "todos", to: "todos#index"
+  #post "todos/create", to: "todos#create"
+  #get "todos/:id", to: "todos#show"
   
+  resources :todos
 end
