c530643347c132b31d206ae5d76dbf0b60152706
vinayrvr987 <vinayrvr987@gmail.com>
2020-04-04 17:38:05 +0530

    can see POST using postman

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "todos", to: "todos#index"
-  get "todos/create", to: "todos#create"
+  post "todos/create", to: "todos#create"
   get "todos/:id", to: "todos#show"
   
 end
