e1f27f12d5f2e78074ef8d19f4d88c8c613cb0fd
vinayrvr987 <vinayrvr987@gmail.com>
2020-04-04 18:00:53 +0530

    PATCH request to mark Todo as complete

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,6 @@
 class TodosController < ApplicationController
   skip_before_action :verify_authenticity_token
-  
+
     def index
       render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string}.join("\n")
     end
@@ -21,5 +21,15 @@ class TodosController < ApplicationController
       )
       response_text = "Hey, your new todo is created with the id #{new_todo.id}"
       render plain: response_text
-    end      
+    end  
+
+    def update 
+      id = params[:id]
+      completed = params[:completed]
+      todo = Todo.find(id)
+      todo.completed = completed
+      todo.save!
+      render plain: "Updated todo completed status to #{todo.completed}"
+    end
+
 end  
