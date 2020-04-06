7b8e78a7de0574415b2fadd4d7dc5a89d05e30fe
vinayrvr987 <vinayrvr987@gmail.com>
2020-04-07 01:01:09 +0530

    Updated database migrations

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,51 @@
 class Todo < ActiveRecord::Base
-  def to_pleasant_string
-    is_completed = completed ? "[X]" : "[]"
-    "#{id}. #{due_date.to_s(:long)}  ,  #{todo_text} #{is_completed}"
+  def overdue?
+    Date.today > due_date
+  end
+
+  def due_today?
+    Date.today == due_date
+  end
+
+  def due_later?
+    Date.today < due_date
+  end
+
+  def self.overdue
+    all.where(" due_date< ?", Date.today)
+  end
+
+  def self.due_today
+    all.where(due_date: Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date> ?", Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
+  end
+
+  def self.show_list
+    puts "My Todo-list\n\n"
+    puts "Overdue\n"
+    puts overdue.map { |todo| todo.to_displayable_string }
+
+    puts "\n\n"
+    puts "Due Today\n"
+    puts due_today.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+
+    puts "Due Later\n"
+    puts due_later.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+  end
+
+  def self.mark_as_complete!(todo_id)
+    todo = Todo.find(todo_id)
+    todo.completed = true
+    todo.save
+    Todo.find(todo_id)
   end
 end
