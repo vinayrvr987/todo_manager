d8ccd204a50248e7c8a0b6807c2d01f4fb3b0260
vinayrvr987 <vinayrvr987@gmail.com>
2020-04-15 11:36:35 +0530

    make app secure by ensuring current user

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,12 +1,12 @@
 class TodosController < ApplicationController
   def index
-    # render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
+    @todo = Todo.of_user(current_user)
     render "index"
   end
 
   def show
     id = params[:id]
-    todo = Todo.find(id)
+    todo = Todo.of_user(current_user).find(id)
     render "todo"
     #render plain: todo.to_pleasant_string
   end
@@ -18,6 +18,7 @@ class TodosController < ApplicationController
       todo_text: todo_text,
       due_date: due_date,
       completed: false,
+      user_id: current_user.id,
     )
     redirect_to todos_path
   end
@@ -25,7 +26,7 @@ class TodosController < ApplicationController
   def update
     id = params[:id]
     completed = params[:completed]
-    todo = Todo.find(id)
+    todo = Todo.of_user(current_user).find(id)
     todo.completed = completed
     todo.save!
     redirect_to todos_path
@@ -33,7 +34,7 @@ class TodosController < ApplicationController
 
   def destroy
     id = params[:id]
-    todo = Todo.find(id)
+    todo = Todo.of_user(current_user).find(id)
     todo.destroy
     redirect_to todos_path
   end
